27196caa03d796d15107f725640cbd60041027c2
wstan-ws <wstan.ws97@gmail.com>
2023-11-02 22:38:24 +0800

    done

---
+++ src/Cards.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cards {
+    
+    private String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
+    private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J" ,"Q", "K", "A"};
+    private List<String> deck = new ArrayList<>();
+
+    public Cards() {
+        
+    }
+
+    public void createNewDeck() {
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(String.format("%s of %s", rank, suit));
+            }
+        }
+
+        for (String card : deck) {
+            System.out.println(card);
+        }
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+
+        for (String card : deck) {
+            System.out.println(card);
+        }
+    }
+
+    public void drawTopCard() {
+        System.out.printf("Top Card Drawn: %s\n", deck.get(0));
+    }
+
+    public void drawRandomCard() {
+        int index = (int)(Math.random() * deck.size());
+        System.out.printf("Random Card Drawn: %s\n", deck.get(index));
+    }
+
+    public int getDeckSize() {
+        return deck.size();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Cards deck = new Cards();
+        Scanner scanner = new Scanner(System.in);
+        boolean isLooping = true;
+
+        System.out.println("Here's a set of poker cards.");
+
+        while (isLooping) {
+            String command = scanner.nextLine().trim().toLowerCase();
+            switch (command) {
+                case "create":
+                    deck.createNewDeck();
+                    break;
+                case "shuffle":
+                    if (deck.getDeckSize() <= 0) {
+                        System.out.println("Please create a new deck before shuffling");
+                    } else {
+                    deck.shuffleDeck();
+                    }
+                    break;
+                case "draw top":
+                    if (deck.getDeckSize() <= 0) {
+                        System.out.println("Please create a new deck before drawing");
+                    } else {    
+                        deck.drawTopCard();
+                        System.out.println("Card has been returned to top of the deck");
+                    }
+                    break;
+                case "draw random":
+                    if (deck.getDeckSize() <= 0) {
+                        System.out.println("Please create a new deck before drawing");
+                    } else {    
+                        deck.drawRandomCard();
+                        System.out.println("Card has been returned to the deck");
+                    }
+                    break;
+                case "end":
+                    isLooping = false;
+                    break;
+                default:
+                    System.out.println("""
+                            Invalid Command. Available Command:
+                            - create
+                            - shuffle
+                            - draw top
+                            - draw random
+                            - end""");
+            }
+        }
+        scanner.close();
+    }
+}
